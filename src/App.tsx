a7fa2da09d702351b96fe4fbc2f113072b548a3f
johncloudd <68559861+johncloudd@users.noreply.github.com>
2022-01-24 12:29:41 -0600

    Update title

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Welcome to Static Web Application POC by John Umeojiako
         </p>
         <a
           className="App-link"
